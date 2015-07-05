8d01a8a43957f80d1052e429bc14ead2b7ad623c
Karnith <mmarino@jcmar.com>
2015-07-05 16:38:14 -0700

    modify to gulp

--- bin/sails-www.js
+++ bin/sails-www.js
@@ -10,42 +10,42 @@ var _ = require('lodash');
 var CaptainsLog = require('captains-log');
 var Sails = require('../lib/app');
 var rconf = require('../lib/app/configuration/rc');
-var __Grunt = require('../lib/hooks/grunt');
+var __Gulp = require('../lib/hooks/gulp');
 var Err = require('../errors');
 
 /**
  * `sails www`
  *
- * Run the `grunt build` task
+ * Run the `gulp build` task
  */
 
 module.exports = function() {
   var log = CaptainsLog(rconf.log);
 
   var wwwPath = nodepath.resolve(process.cwd(), './www'),
-    GRUNT_TASK_NAME = 'build',
-    GRUNT_TASK_PROD_NAME = 'buildProd';
+    GULP_TASK_NAME = 'build',
+    GULP_TASK_PROD_NAME = 'buildProd';
 
   var sails = Sails();
   sails.load(_.merge({}, rconf, {
     hooks: {
-      grunt: false
+      gulp: false
     },
     globals: false
   }), function sailsReady(err) {
     if (err) return Err.fatal.failedToLoadSails(err);
 
-    var overrideGruntTask = (sails.config.environment == 'production' ? GRUNT_TASK_PROD_NAME : GRUNT_TASK_NAME)
+    var overrideGulpTask = (sails.config.environment == 'production' ? GULP_TASK_PROD_NAME : GULP_TASK_NAME)
 
-    // Run Grunt task
-    var Grunt = __Grunt(sails);
+    // Run Gulp task
+    var Gulp = __Gulp(sails);
 
-    log.info('Compiling assets into standalone `www` directory with `grunt ' + overrideGruntTask + '`...');
-    Grunt.runTask(overrideGruntTask);
+    log.info('Compiling assets into standalone `www` directory with `gulp ' + overrideGulpTask + '`...');
+    Gulp.runTask(overrideGulpTask);
 
     // Bind error event
-    sails.on('hook:grunt:error', function(err) {
-      log.error('Error occured starting `grunt ' + overrideGruntTask + '`');
+    sails.on('hook:gulp:error', function(err) {
+      log.error('Error occured starting `gulp ' + overrideGulpTask + '`');
       log.error('Please resolve any issues and try running `sails www` again.');
       log.error(err);
       process.exit(1);
@@ -53,7 +53,7 @@ module.exports = function() {
 
     // Task is not actually complete yet-- it's just been started
     // We'll bind an event listener so we know when it is
-    sails.on('hook:grunt:done', function() {
+    sails.on('hook:gulp:done', function() {
       log.info();
       log.info('Created `www` directory at:');
       log.info(wwwPath);
